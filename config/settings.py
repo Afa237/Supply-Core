9debff438dc9fbdca61469874bed6f21a23628b3
Afa237 <atemnkengflorence178@gmail.com>
2026-08-07 15:34:31 +0200

    modified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n4r3v1mwcksr7j7t=)&x9^-c^yr&e!cwphf$=zafau-zq59#8s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
